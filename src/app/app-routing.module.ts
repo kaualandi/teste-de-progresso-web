6cc9ba324032039ab2ea7ef33d47f78774452093
Kauã Landi <eu@kaualf.com>
2023-12-16 10:19:40 -0300

    feat: removed cli generate spec files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { authGuard } from './guards/auth.guard';
 
 const SPR = false;
 
@@ -19,7 +18,7 @@ const routes: Routes = [
   {
     path: '',
     component: NavbarComponent,
-    canActivate: [authGuard], // ? Remover caso queira que mais rotas sejam acessadas sem autenticação
+    // canActivate: [authGuard], // ? Remover caso queira que mais rotas sejam acessadas sem autenticação
     children: [
       {
         path: '',
