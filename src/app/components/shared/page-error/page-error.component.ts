dab8ef2e6d2a5b5261ed72732b781acc8816016b
Kauã Landi <eu@kaualf.com>
2023-12-27 18:58:17 -0300

    fix: 404 page error and fix: module cli generator

--- src/app/components/shared/page-error/page-error.component.ts
+++ src/app/components/shared/page-error/page-error.component.ts
@@ -5,6 +5,7 @@ import {
   OnChanges,
   OnInit,
 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'page-error',
@@ -13,11 +14,14 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageErrorComponent implements OnInit, OnChanges {
+  constructor(private route: ActivatedRoute) {}
+
   @Input() code = 0;
 
   translated = 'default';
 
   ngOnInit() {
+    this.code ||= this.route.snapshot.data['code'];
     this.translated = [500, 404, 403, 401, 400].includes(this.code)
       ? this.code.toString()
       : 'default';
