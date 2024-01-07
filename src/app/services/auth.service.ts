c38bae2b2b6932d573a69f9b957d76a458b69d84
Kauã Landi <eu@kaualf.com>
2024-01-06 22:12:43 -0300

    fix: auth guard, prettier and typograph

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   ) {}
 
   login(body: BodyJson) {
-    body['password'] = Md5.init(body['password']);
+    body['password'] = Md5.init(body['password']).toUpperCase();
     return this.http.post<IToken>('core/auth/', body);
   }
 
