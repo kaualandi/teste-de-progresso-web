01db710d622194062949585061be630784b39b3b
Kauã Landi <eu@kaualf.com>
2024-01-07 15:25:37 -0300

    fix: authGuard retirect login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
 
 export const authGuard: CanActivateFn = () => {
   const storage = inject(StorageService);
+  const router = inject(Router);
   const token = storage.token;
   if (!token) {
+    router.navigate(['/login']);
     return false;
   }
   return true;
