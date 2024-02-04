69e5e687890938ef80057291ce3950f742043a54
Kauã Landi <eu@kaualf.com>
2024-02-04 12:27:56 -0300

    feat: add select page in paginator

--- src/app/components/shared/pagination/pagination.component.ts
+++ src/app/components/shared/pagination/pagination.component.ts
@@ -1,16 +1,44 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit, OnChanges {
   @Input() back = false;
   @Input() next = false;
   @Input() current = 1;
   @Input() total = 1;
   @Output() page_change = new EventEmitter<number>();
+
+  currentControl = new FormControl(1);
+
+  ngOnInit(): void {
+    this.currentControl.valueChanges
+      .pipe(debounceTime(1000), distinctUntilChanged())
+      .subscribe((value) => {
+        if (!value) return;
+        if (value > this.total) {
+          this.currentControl.setValue(this.total);
+          return;
+        }
+        this.page_change.emit(value);
+      });
+  }
+
+  ngOnChanges() {
+    this.currentControl.setValue(this.current, { emitEvent: false });
+  }
 
   handlePageChange(page: number) {
     this.page_change.emit(page);
