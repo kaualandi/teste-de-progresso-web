450c8c718f1d2f872a0ac1eba6eaa8b774af26ad
Kauã Landi <eu@kaualf.com>
2024-08-10 19:21:32 -0300

    refactor: optimize route animations for better performance

--- src/app/animations/route-animation.ts
+++ src/app/animations/route-animation.ts
@@ -20,7 +20,7 @@ export const slideInAnimation = trigger('routeAnimations', [
             transform: 'translateX(100%) scale(0.8)',
           }),
           animate(
-            '0.5s ease-in-out',
+            '0.3s ease-in-out',
             style({
               transform: 'translateX(0%) scale(1)',
             })
@@ -35,7 +35,7 @@ export const slideInAnimation = trigger('routeAnimations', [
             transform: 'scale(1)',
           }),
           animate(
-            '0.5s ease-in-out',
+            '0.3s ease-in-out',
             style({
               transform: 'scale(0.8)',
             })
@@ -51,14 +51,14 @@ export const zoomInAnimation = trigger('zoomInAnimation', [
   transition(':enter', [
     style({ transform: 'scale(0.5)', opacity: 0 }),
     animate(
-      '1s cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+      '0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)',
       style({ transform: 'scale(1)', opacity: 1 })
     ),
   ]),
   transition(':leave', [
     style({ transform: 'scale(1)', opacity: 1 }),
     animate(
-      '1s cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+      '0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)',
       style({ transform: 'scale(0.5)', opacity: 0 })
     ),
   ]),
